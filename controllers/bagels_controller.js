796251daeb5f2d1193eafe7903a6b51955b12ae5
Jessica D <deck.jessica@gmail.com>
2021-01-29 21:18:59 -0500

    finished connection.js, started working on orm.js, and setting up model for bagels, starting to look at controller side

---
+++ controllers/bagels_controller.js
@@ -0,0 +1,9 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+var bagel = require("../models/bagel.js");
+
+
+module.exports = router;
---
+++ models/bagel.js
@@ -0,0 +1,21 @@
+var orm = require("../config/orm.js");
+
+var bagel = {
+    selectAll: function(cb) {
+        orm.all("bagels", function(res) {
+          cb(res);
+        });
+      },
+      insertOne: function(cols, vals, cb) {
+        orm.create("bagels", cols, vals, function(res) {
+          cb(res);
+        });
+      },
+      update: function(objColVals, condition, cb) {
+        orm.update("bagels", objColVals, condition, function(res) {
+          cb(res);
+        });
+      },
+};
+
+module.exports = bagel;
